4d3b8c479388d5338df84c8de4d2d1a44cdd8132
jcw1031 <jcw001031@gmail.com>
2024-02-11 17:35:52 +0900

    initial commit

---
+++ src/main/java/team/a5/gachigayu/GachigayuApplication.java
@@ -0,0 +1,13 @@
+package team.a5.gachigayu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GachigayuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GachigayuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/team/a5/gachigayu/GachigayuApplicationTests.java
@@ -0,0 +1,13 @@
+package team.a5.gachigayu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GachigayuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
