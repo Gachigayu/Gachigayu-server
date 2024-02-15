16a1a9f23916378c54bee63bd36d01996a394ff4
jcw1031 <jcw001031@gmail.com>
2024-02-15 17:12:25 +0900

    fix: routes response add description

--- src/main/java/team/a5/gachigayu/controller/dto/response/RouteResponse.java
+++ src/main/java/team/a5/gachigayu/controller/dto/response/RouteResponse.java
@@ -3,11 +3,11 @@ package team.a5.gachigayu.controller.dto.response;
 import org.locationtech.jts.geom.Point;
 import team.a5.gachigayu.domain.Route;
 
-public record RouteResponse(int sequence, double latitude, double longitude) {
+public record RouteResponse(int sequence, double latitude, double longitude, String description) {
 
     public static RouteResponse of(Route route) {
         int sequence = route.getSequence();
         Point coordinate = route.getCoordinate();
-        return new RouteResponse(sequence, coordinate.getX(), coordinate.getY());
+        return new RouteResponse(sequence, coordinate.getX(), coordinate.getY(), route.getDescription());
     }
 }
