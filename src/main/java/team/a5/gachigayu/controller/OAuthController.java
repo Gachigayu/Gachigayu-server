fb8a34087d886dcf604a647397c371fc113c4d8c
jcw1031 <jcw001031@gmail.com>
2024-02-16 10:48:37 +0900

    fix: redirect path

--- src/main/java/team/a5/gachigayu/controller/OAuthController.java
+++ src/main/java/team/a5/gachigayu/controller/OAuthController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 import team.a5.gachigayu.domain.value.SignUpResult;
 
 import java.io.IOException;
@@ -15,9 +16,15 @@ public class OAuthController {
 
     @GetMapping
     public void oauth2Redirect(
-            @RequestParam String accessToken, @RequestParam String refreshToken,
+            @RequestParam("access_token") String accessToken, @RequestParam("refresh_token") String refreshToken,
             @RequestParam SignUpResult account, HttpServletResponse response
     ) throws IOException {
-        response.sendRedirect("/oauth2/authorization");
+        String uri = UriComponentsBuilder.fromUriString("/oauth2/authorization")
+                .queryParam("access_token", accessToken)
+                .queryParam("refresh_token", refreshToken)
+                .queryParam("account", account)
+                .build()
+                .toUriString();
+        response.sendRedirect(uri);
     }
 }
